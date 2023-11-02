d6984388f9b441d1e8cc3c31173777a18ac81a89
Vanitaaa07 <23ucs729@lnmiit.ac.in>
2023-11-03 00:12:19 +0530

    done

--- script.js
+++ script.js
@@ -24,8 +24,46 @@ document.addEventListener('DOMContenLoaded', ()=>{
 
           }
         };
-
+        
+        const recordMove = (move,count) => {
+            const formattedMove = count % 2 === 1? '$(Math.ceil
+               (count / 2)}. ${move}' : '${move} -';
+               moveHistroy.textContent += formattedMove + ' ';
+               moveHistory.scrollTop = moveHistory,scrollHeight;
        
 
-    }
+    };
+      const onDragStart = (source,piece) => {
+        return !game.game_over() && PictureInPictureEvent.search(usercolor) ===
+        0;
+      };
+      const onDrop = (source, target) =>{
+         const move = game.move({
+            from: source,
+            to: target,
+            promotion: 'q',
+         });
+         if(move === null) return 'snapback';
+
+         window.setTimeout(makeRandomMove, 250);
+         recordMove(move.san, moveCount);
+         moveCount++;
+
+      };
+
+      const onSnapEnd = () =>{
+        board.position(game.fen());
+      }
+
+      const boardConfig = {
+        showNotation: true,
+        draggable: true,
+        position: 'start',
+        onDragStart,
+        onDrop,
+        onSnapEnd,
+        moveSpeed: 'fast',
+        
+      }
+
 }
